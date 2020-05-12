4b7e8a42fc6903770ceabe1b98dfbcb35cdb42bf
rdecelis <rdecelis@stt-systems.com>
2020-05-12 11:34:08 +0200

    Add Travis progressed_test0

---
+++ .travis/progressed_test0.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+import subprocess, time
+
+p = subprocess.Popen("python progressed-py/progressed.py", shell=True)
+time.sleep(10)
+p.kill()
+p.wait()
+print("\nReturned Code:\n")
+if p.returncode == -9:
+    print(0)
+    exit(0)
+else:
+    print(p.returncode)
+    exit(p.returncode)
